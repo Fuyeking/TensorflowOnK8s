79ba23c2e937050115e8ab5a313d6e52b1a94233
Fuyeking <474387803@qq.com>
2018-11-23 23:38:03 +0800

    python3.0

---
+++ service/disTf.py
@@ -0,0 +1,34 @@
+1# -*- coding: utf-8 -*-
+import sys
+import os
+sys.path.append(os.path.split(os.path.realpath(__file__))[0]+"/..")
+import tornado
+from tornado.web import Application
+from util.ApiConfiger import ApiConfig
+from handlers.train_handler import TrainHandler
+import logging
+from logging.config import fileConfig
+
+class DisDeepService(object):
+    def __init__(self):
+        self.config = ApiConfig()
+
+    def start(self):
+        app = tornado.web.Application([
+            (r"/v1/train", TrainHandler)
+            ])
+        app.listen(self.config.getint("service", "port"))
+        logging.info("service start ...")
+        tornado.ioloop.IOLoop.current().start()
+
+if __name__ == '__main__':
+    wp = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    if not os.path.exists(wp+'/logs'):
+        os.mkdir(wp+'/logs')
+    print(wp)
+    fileConfig(wp+'/conf/logging.conf')
+    logger = logging.getLogger()
+    logger.info('start ...')
+    service = DisDeepService()
+    print ("start ....")
+    service.start()
